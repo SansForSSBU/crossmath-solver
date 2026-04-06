5f8283a906347a34bc865954d94014d4f134ac8d
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-06 18:03:33 +0100

    Add checks for all of a number being used up in the solver

--- src/solver.py
+++ src/solver.py
@@ -2,6 +2,7 @@ import cv2
 from src.reader import read_img, print_grid
 import operator
 import itertools
+from collections import Counter
 
 class Equation():
     def __init__(self, terms):
@@ -52,17 +53,22 @@ def find_equations(row):
             eq.append(Equation(curr_eq))
     return eq
 
-
-def solve(available_nums, grid):
+def grid_to_equations(grid):
     equations = []
     for row in grid:
         equations.extend(find_equations(row))
 
     for col in grid.T:
         equations.extend(find_equations(col))
+    
+    return equations
+
+def solve(available_nums, grid):
+    equations = grid_to_equations(grid)
 
     candidates = {f"n{idx+1}": list(set(available_nums.copy())) for idx, _ in enumerate(available_nums)}
     while True:
+        # Filtering: for each equation, constrain the candidates to what could work.
         for curr_term,_ in candidates.items():
             for equation in equations:
                 terms = equation.get_terms()
@@ -77,6 +83,23 @@ def solve(available_nums, grid):
 
                 valid_combinations = [comb for comb in all_combinations if equation.check(comb)]
                 candidates[curr_term] = list(set([comb[curr_term] for comb in valid_combinations]))
+        
+        # If all of a number are used up, don't allow other candidates to use it.
+        available_qtys = Counter(available_nums)
+        nums_used = {}
+        for cand in candidates.values():
+            if len(cand) == 1:
+                num_used = cand[0]
+                nums_used[num_used] = nums_used.get(num_used, 0) + 1
+        
+        pass
+        for k in nums_used.keys():
+            if nums_used[k] == available_qtys[k]:
+                for cand in candidates.values():
+                    if len(cand) != 1:
+                        if k in cand:
+                            cand.remove(k)
+
         if all(len(cand) <= 1 for cand in candidates.values()):
             break
 
