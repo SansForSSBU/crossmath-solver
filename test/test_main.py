3dbd125449f7603435888e8613b11b9d63494ca6
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-06 15:10:44 +0100

    Add test to ensure the solution remains the same

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,14 @@
 from src.main import solve_img
+import numpy as np
+from pathlib import Path
+import os
+
 def test_puzzles():
     img_path = "./test/puzzles/1.png"
     ans = solve_img(img_path)
-    
+    solution_path = "./test/solutions/1.npy"
+    if os.path.exists(Path("./test/solutions/1.npy")):
+        old_ans = np.load(solution_path)
+        assert np.array_equal(ans, old_ans)
+    else:
+        np.save(solution_path, ans)
