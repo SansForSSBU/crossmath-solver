6242306b1c88397e069b77b054ea043fca4795a3
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-04-06 21:14:56 +0100

    Allow prepare_for_ocr to return different images to give to the operator detector and number detector

--- src/reader.py
+++ src/reader.py
@@ -80,25 +80,33 @@ def prepare_for_ocr(crop, inset=6):
     crop = crop[inset: -inset, inset: -inset]    
     gray_crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     resized_crop = cv2.resize(gray_crop, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    _, clean_crop = cv2.threshold(resized_crop, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    return clean_crop
+    blurred_crop = cv2.medianBlur(resized_crop, 7)
+    _, op_crop = cv2.threshold(blurred_crop, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, num_crop = cv2.threshold(resized_crop, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return (op_crop, num_crop)
 
 reader = easyocr.Reader(['en'])
-def do_ocr(img, can_be_operator=True):
-    img2 = Image.fromarray(img)
-    img2 = ImageOps.expand(img2, border=(0, 10, 0, 10), fill='white')
+image_idx = 0
+def do_ocr(img, can_be_operator=True, debug=True):
+    op_crop, num_crop = img
+    global image_idx
+    op_crop = Image.fromarray(op_crop)
+    op_crop = ImageOps.expand(op_crop, border=(0, 10, 0, 10), fill='white')
+    if debug:
+        op_crop.save(f"test_images/{image_idx}.png")
+        image_idx += 1
     if can_be_operator:
-        result = pytesseract.image_to_string(img2, config=r'--oem 3 --psm 10 -c tessedit_char_whitelist=')
+        result = pytesseract.image_to_string(op_crop, config=r'--oem 3 --psm 10 -c tessedit_char_whitelist=')
         result = result.strip()
         if result == '/':
             return result
-        result = pytesseract.image_to_string(img2, config=r'--oem 3 --psm 13 -c tessedit_char_whitelist=+-x=')
+        result = pytesseract.image_to_string(op_crop, config=r'--oem 3 --psm 13 -c tessedit_char_whitelist=+-x=')
         result = result.strip()
         if result in ['+', '-', 'x', '=']:
             if result == 'x':
                 return '*'
             return result
-    results = reader.readtext(img, detail=0, paragraph=False, rotation_info=[0], allowlist='0123456789 ')
+    results = reader.readtext(num_crop, detail=0, paragraph=False, rotation_info=[0], allowlist='0123456789 ')
     if len(results) > 0:
         return results[0]
     return ''
